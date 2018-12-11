f8d80e0e5a8284134c21f185e095f4b5e33822a7
Ferran Basora <fcsonline@gmail.com>
2018-12-11 23:21:50 +0000

    WIP

--- src/actions/request.rs
+++ src/actions/request.rs
@@ -1,17 +1,14 @@
 use std::collections::HashMap;
-use std::io::Read;
+use std::io::{self, Write};
 
 use yaml_rust::Yaml;
 use colored::*;
 use serde_json;
-use time;
+// use time;
 
-use hyper::client::{Client, Response};
-use hyper::net::HttpsConnector;
-use hyper_native_tls::NativeTlsClient;
-use hyper::header::{UserAgent, Headers, Cookie, SetCookie};
-use hyper::method::Method;
-use hyper_native_tls::native_tls::TlsConnector;
+use hyper::{Client, Method, Body};
+use hyper::header::HeaderName;
+use hyper::rt::{Future, Stream};
 
 use interpolator;
 use config;
@@ -72,21 +69,15 @@ impl Request {
     }
   }
 
-  fn send_request(&self, context: &mut HashMap<String, Yaml>, responses: &mut HashMap<String, serde_json::Value>, config: &config::Config) -> (Response, f64) {
-    // Build a TSL connector
-    let mut connector_builder = TlsConnector::builder();
-    connector_builder.danger_accept_invalid_certs(config.no_check_certificate);
-
-    let ssl = NativeTlsClient::from(connector_builder.build().unwrap());
-    let connector = HttpsConnector::new(ssl);
-    let client = Client::with_connector(connector);
+  fn send_request(&self, context: &mut HashMap<String, Yaml>, responses: &mut HashMap<String, serde_json::Value>, _config: &config::Config) {
+    let client = Client::new();
 
     let begin = time::precise_time_s();
 
     let interpolated_name;
     let interpolated_url;
-    let interpolated_body;
-    let request;
+    // let interpolated_body;
+    // let request;
 
     // Resolve the url
     {
@@ -97,92 +88,138 @@ impl Request {
 
     // Method
     let method = match self.method.to_uppercase().as_ref() {
-      "GET" => Method::Get,
-      "POST" => Method::Post,
-      "PUT" => Method::Put,
-      "PATCH" => Method::Patch,
-      "DELETE" => Method::Delete,
+      "GET" => Method::GET,
+      "POST" => Method::POST,
+      "PUT" => Method::PUT,
+      "PATCH" => Method::PATCH,
+      "DELETE" => Method::DELETE,
       _ => panic!("Unknown method '{}'", self.method),
     };
 
-    // Body
-    if let Some(body) = self.body.as_ref() {
-      // Resolve the body
-      let interpolator = interpolator::Interpolator::new(context, responses);
-      interpolated_body = interpolator.resolve(body).to_owned();
+    let mut builder = hyper::Request::builder();
 
-      request = client
-        .request(method, &interpolated_url)
-        .body(&interpolated_body);
-    } else {
-      request = client.request(method, &interpolated_url);
-    }
+    builder.uri(interpolated_url.clone());
+    builder.method(method);
+
+    // let req = builder.body(Body::from(""));
+
+    // Body
+    // if let Some(body) = self.body.as_ref() {
+    //   // Resolve the body
+    //   let interpolator = interpolator::Interpolator::new(context, responses);
+    //   interpolated_body = interpolator.resolve(body).to_owned();
+
+    //   request = client
+    //     .method(method)
+    //     .uri(&interpolated_url)
+    //     .body(&interpolated_body);
+    // } else {
+    //   request = client.request(method, &interpolated_url);
+    // }
 
     // Headers
-    let mut headers = Headers::new();
-    headers.set(UserAgent(USER_AGENT.to_string()));
+    builder.header(hyper::header::USER_AGENT, USER_AGENT.to_string());
 
     if let Some(cookie) = context.get("cookie") {
-      headers.set(Cookie(vec![String::from(cookie.as_str().unwrap())]));
+      builder.header(
+        hyper::header::COOKIE,
+        cookie.as_str().unwrap()
+      );
     }
 
     for (key, val) in self.headers.iter() {
       // Resolve the body
       let interpolator = interpolator::Interpolator::new(context, responses);
-      let interpolated_header = interpolator.resolve(val).to_owned();
+      let interpolated_header = interpolator.resolve(val);
 
-      headers.set_raw(key.to_owned(), vec![interpolated_header.clone().into_bytes()]);
+      builder.header(
+        HeaderName::from_lowercase(key.as_bytes()).unwrap(),
+        interpolated_header
+      );
     }
 
-    let response_result = request.headers(headers).send();
+    let req = builder.body(Body::from(""));
 
-    if let Err(e) = response_result {
-      panic!("Error connecting '{}': {:?}", interpolated_url, e);
-    }
+    let future = client
+      .request(req.unwrap())
+      .and_then(move |res| {
+          let duration_ms = (time::precise_time_s() - begin) * 1000.0;
+          let status = res.status();
 
-    let response = response_result.unwrap();
-    let duration_ms = (time::precise_time_s() - begin) * 1000.0;
+          let status_text = if status.is_server_error() {
+            status.to_string().red()
+          } else if status.is_client_error() {
+            status.to_string().purple()
+          } else {
+            status.to_string().yellow()
+          };
 
-    let status_text = if response.status.is_server_error() {
-      response.status.to_string().red()
-    } else if response.status.is_client_error() {
-      response.status.to_string().purple()
-    } else {
-      response.status.to_string().yellow()
-    };
+          println!("{:width$} {} {} {}{}", interpolated_name.green(), interpolated_url.blue().bold(), status_text, duration_ms.round().to_string().cyan(), "ms".cyan(), width=25);
 
-    println!("{:width$} {} {} {}{}", interpolated_name.green(), interpolated_url.blue().bold(), status_text, duration_ms.round().to_string().cyan(), "ms".cyan(), width=25);
+          // The body is a stream, and for_each returns a new Future
+          // when the stream is finished, and calls the closure on
+          // each chunk of the body...
+          res.into_body().for_each(|chunk| {
+              io::stdout().write_all(&chunk)
+                  .map_err(|e| panic!("example expects stdout is open, error={}", e))
+          })
+      })
+      .map_err(|err| {
+          eprintln!("Error {}", err);
+      });
 
-    (response, duration_ms)
+    // let future = client
+    //   .request(request.unwrap())
+    //   .and_then(|res| {
+    //     println!("Response: {}", res.status());
+    //   });
+
+      // .map(|_| {
+      //   println!("\n\nDone.");
+      // })
+      // .map_err(|err| {
+      //   eprintln!("Error {}", err);
+      // });
+
+    hyper::rt::run(future);
+
+    // if let Err(e) = response_result {
+    //   panic!("Error connecting '{}': {:?}", interpolated_url, e);
+    // }
+
+    // let response = response_result.unwrap();
+
+
+    // (response, duration_ms)
   }
 }
 
 impl Runnable for Request {
-  fn execute(&self, context: &mut HashMap<String, Yaml>, responses: &mut HashMap<String, serde_json::Value>, reports: &mut Vec<Report>, config: &config::Config) {
+  fn execute(&self, context: &mut HashMap<String, Yaml>, responses: &mut HashMap<String, serde_json::Value>, _reports: &mut Vec<Report>, config: &config::Config) {
     if self.with_item.is_some() {
       context.insert("item".to_string(), self.with_item.clone().unwrap());
     }
 
-    let (mut response, duration_ms) = self.send_request(context, responses, config);
+    self.send_request(context, responses, config);
 
-    reports.push(Report { name: self.name.to_owned(), duration: duration_ms, status: response.status.to_u16() });
+    // reports.push(Report { name: self.name.to_owned(), duration: duration_ms, status: response.status.to_u16() });
 
-    if let Some(&SetCookie(ref cookies)) = response.headers.get::<SetCookie>() {
-      if let Some(cookie) = cookies.iter().next() {
-        let value = String::from(cookie.split(";").next().unwrap());
-        context.insert("cookie".to_string(), Yaml::String(value));
-      }
-    }
+    // if let Some(&SetCookie(ref cookies)) = response.headers.get::<SetCookie>() {
+    //   if let Some(cookie) = cookies.iter().next() {
+    //     let value = String::from(cookie.split(";").next().unwrap());
+    //     context.insert("cookie".to_string(), Yaml::String(value));
+    //   }
+    // }
 
-    if let Some(ref key) = self.assign {
-      let mut data = String::new();
+    // if let Some(ref key) = self.assign {
+    //   let mut data = String::new();
 
-      response.read_to_string(&mut data).unwrap();
+    //   response.read_to_string(&mut data).unwrap();
 
-      let value: serde_json::Value = serde_json::from_str(&data).unwrap();
+    //   let value: serde_json::Value = serde_json::from_str(&data).unwrap();
 
-      responses.insert(key.to_owned(), value);
-    }
+    //   responses.insert(key.to_owned(), value);
+    // }
   }
 
 }
