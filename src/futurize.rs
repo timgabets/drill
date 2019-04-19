6b5ae7b9c13d2d2785a28eb1e3e2c3223b7fb9ab
Ferran Basora <fcsonline@gmail.com>
2019-04-19 12:25:50 +0000

    WIP

--- src/futurize.rs
+++ src/futurize.rs
@@ -4,6 +4,44 @@ use crate::actions::Runnable;
 use crate::config;
 
 pub fn build(benchmark: Arc<Vec<Box<(Runnable + Sync + Send)>>>, config: Arc<config::Config>) -> impl Future<Item=(), Error=()> {
+
+    // let client = hyper::Client::new();
+    // client
+    //   .get("http://localhost:9000/api/organizations".parse().unwrap())
+    //   .map_err(|err| {
+    //     println!("Error: {}", err);
+    //   })
+
+    // benchmark
+    //   .iter()
+    //   .for_each(|def| {
+    //     let client = hyper::Client::new();
+
+    //     let req = client
+    //       .get("http://localhost:9000/api/organizations".parse().unwrap())
+    //       .and_then(|resp| {
+    //         println!("Status: {}", resp.status());
+
+    //         previous.unwrap_or(last_ok)
+    //       });
+
+    //     previous = Some(req);
+    //   });
+
+    // let all = benchmark
+    //   .iter()
+    //   .map(|def| {
+    //     let client = hyper::Client::new();
+
+    //     client
+    //       .get("http://localhost:9000/api/organizations".parse().unwrap())
+    //       .map_err(|err| {
+    //         println!("Error: {}", err);
+    //       })
+    //   });
+
+    // futures::future::join_all(all).then(|a| { () })
+
     let client = hyper::Client::new();
     let f1 = client
       .get("http://localhost:9000/api/users.json".parse().unwrap())
